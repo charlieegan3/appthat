29a5bf7f3817377c8eff2c4aa1498834526ca146
Charlie Egan <charlieegan3@googlemail.com>
2015-06-16 23:23:17 +0100

    Remove old channels route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#index'
-  get '/channels' => 'application#channels'
   get 'flag/:id' => 'application#flag'
 
   # Example of regular route:
